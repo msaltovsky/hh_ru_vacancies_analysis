f77f49f24d503b3f61262d273c015b168b9d4b71
msaltovsky <mesaltovskiy_1@edu.hse.ru>
2024-05-29 22:14:47 +0300

    предфинальный блокнот
    функция визуализации результатов регрессии вынесена в модуль visualization.py

--- python_modules/visualization.py
+++ python_modules/visualization.py
@@ -35,3 +35,39 @@ def visualize_avg_salary_in_moscow(given_datasets, mo_gdf):
         plt.xticks([])
         plt.yticks([])
         plt.show()
+
+
+def plot_yreal_ypred(y_test, y_train, y_test_hat, y_train_hat):
+    """
+        Рисует картинку для прогнозов регрессии. Взят из андана 3 домашняя.
+    """
+
+    margin = 0.1  # отступ на границах
+    plt.figure(figsize=(10, 5))
+    plt.subplot(121)
+    plt.scatter(y_train, y_train_hat, color="red", alpha=0.5)
+    plt.xlabel('Истинные значения')
+    plt.ylabel('Предсказанные значения')
+    plt.axis('equal')
+    plt.axis('square')
+    train_min = min(y_train)
+    train_max = max(y_train)
+    plt.xlim(train_min - margin, train_max + margin)
+    plt.ylim(train_min - margin, train_max + margin)
+    plt.plot([train_min - margin, train_max + margin], [train_min - margin, train_max + margin])
+    plt.title('Train set', fontsize=20)
+
+    plt.subplot(122)
+    plt.scatter(y_test, y_test_hat, color="red", alpha=0.5)
+    plt.xlabel('Истинные значения')
+    plt.ylabel('Предсказанные значения')
+    plt.axis('equal')
+    plt.axis('square')
+    test_min = min(y_test)
+    test_max = max(y_test)
+
+    plt.xlim(test_min - margin, test_max + margin)
+    plt.ylim(test_min - margin, test_max + margin)
+    plt.plot([test_min - margin, test_max + margin], [test_min - margin, test_max + margin])
+    plt.title('Test set', fontsize=20)
+    pass
